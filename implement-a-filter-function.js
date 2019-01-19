58c1ea22ee57c4d308e32c1c584c14ca8cb48110
Sacret <sacret190990@gmail.com>
2019-01-19 23:39:23 +0300

    Add bunch of solutions for codewars v16

---
+++ implement-a-filter-function.js
@@ -0,0 +1,14 @@
+/**
+  * What we want to implement is a filter function, like Array.filter(), also similar to the _.filter() in underscore.js and lodash.js.
+  *
+  * The usage is quite simple, like:
+  * 
+  * [1,2,3,4].filter((num)=>{ return num > 3})
+  * should output [4]
+**/
+
+Array.prototype.filter = function(func) {
+  return this.reduce(function(arr, elem) {
+    return func(elem) ? [...arr, elem] : arr;
+  }, []);
+}
